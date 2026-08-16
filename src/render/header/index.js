dee773286b57fbc595cda640cf25addd2bed6b55
jiangsuiting <1158973435@qq.com>
2026-08-16 09:20:34 +0800

    refactor(render): 移除表头渲染相关模型和工具类
    - 删除 BorderMask 边框掩码类及其位域定义
    - 移除 Fragment 可视片段类及相关的构造参数定义
    - 移除 FrozenBoundaryInfo 冻结边界信息类
    - 删除 HeaderLayoutBuilder 表头布局构建器类
    - 移除 HeaderPainter 表头绘制器类及其绘制逻辑
    - 更新 header 模块导出文件，移除相关导出项

--- src/render/header/index.js
+++
@@ -1,6 +0,0 @@
-export { HeaderLayoutBuilder } from "./HeaderLayoutBuilder.js";
-export { HeaderPainter } from "./HeaderPainter.js";
-export { BorderMask } from "./models/BorderMask.js";
-export { LogicalCell } from "./models/LogicalCell.js";
-export { Fragment } from "./models/Fragment.js";
-export { FrozenBoundaryInfo } from "./models/FrozenBoundaryInfo.js";
--- src/render/header/models/PartialType.js
+++
@@ -1,5 +0,0 @@
-export const PARTIAL_TYPE = Object.freeze({
-    FULL: "full",
-    FROZEN: "frozen",
-    SCROLL: "scroll",
-});
